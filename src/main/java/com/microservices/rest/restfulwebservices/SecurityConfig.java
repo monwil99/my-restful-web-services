730fdb8dcf7ae7d93f1099a7b49f7e568ed7cd44
Wilmon <monwil_99@yahoo.com>
2018-11-15 11:37:31 +0800

    nov 15 update

--- src/main/java/com/microservices/rest/restfulwebservices/SecurityConfig.java
+++ src/main/java/com/microservices/rest/restfulwebservices/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.microservices.rest.restfulwebservices;
+/*package com.microservices.rest.restfulwebservices;
  
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,4 +10,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
     }
-}
+}*/
