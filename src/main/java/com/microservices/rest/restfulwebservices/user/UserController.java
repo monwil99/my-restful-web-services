a7140238960b1a1ba3521ef230aa6dfcbe7c299c
Wilmon Monteros <wilmonM@SourcefitMNL.com>
2018-11-26 20:53:55 +0800

    nov 26 update

--- src/main/java/com/microservices/rest/restfulwebservices/user/UserController.java
+++ src/main/java/com/microservices/rest/restfulwebservices/user/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -79,6 +82,16 @@ public class UserController {
 		}
 		return user;
 		
+	}
+	
+	
+	@GetMapping("/sample/monwil")
+	public String paramterizedUrl( @RequestParam("name") String pangalan,
+			@RequestParam("surname") String apelyido
+	) { 
+		
+		String yourName = "Your name is " + pangalan + " " + apelyido;
+		return yourName;
 	}
 
 }
